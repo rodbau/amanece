df0f690a4e88b374de435321c1113348b7ab947b
Rodrigo <rodbau@users.noreply.github.com>
2021-04-11 12:55:45 -0300

    updates

--- index.php
+++ index.php
@@ -234,7 +234,7 @@
                 <a href="images/slider-4.jpg" data-fancybox="images" data-caption="Caption for this image"><img src="images/slider-4.jpg" alt="Image placeholder" class="img-fluid"></a>
               </div>
               <div class="slider-item">
-                <a href="images/video.mp4" data-fancybox="images" data-caption="Caption for this image"><video src="images/video.mp4" alt="Image placeholder" class="img-fluid"></a>
+                <a href="images/slider-4.jpg" data-fancybox="images" data-caption="Caption for this image"><img src="images/video.mp4" alt="Image placeholder" class="img-fluid"></a>
               </div>
             </div>
             <!-- END slider -->
