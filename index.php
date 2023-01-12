0bb18e43de33783a0c11cbc11ff3daf6296e764c
Micaela <micasolflores@gmail.com>
2023-01-11 20:22:24 -0500

    update

--- index.php
+++ index.php
@@ -221,7 +221,7 @@
             <div class="home-slider major-caousel owl-carousel mb-5" data-aos="fade-up" data-aos-delay="200">
               
               <div class="slider-item">
-                <a href="images/slider-1.jpg" data-fancybox="images" data-caption="Caption for this image"><img src="images/slider-1.jpg" alt="Image placeholder" class="img-fluid"></a>
+                <a href="images/slider-1.png" data-fancybox="images" data-caption="Caption for this image"><img src="images/slider-1.png" alt="Image placeholder" class="img-fluid"></a>
               </div>
               <div class="slider-item">
                 <a href="images/slider-2.jpg" data-fancybox="images" data-caption="Caption for this image"><img src="images/slider-2.jpg" alt="Image placeholder" class="img-fluid"></a>
